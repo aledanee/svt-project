1ce271da5d9440d397f588f977456ee6ba76c823
coder <coder@ibrahims-MacBook-Air.local>
2023-11-06 10:21:47 +0300

    fromme

--- backEnd/DBconnectin/db.java
+++ backEnd/DBconnectin/db.java
@@ -1,3 +1,75 @@
+import java.sql.*;
+
 public class db {
    
+    private Connection connection;
+    private String url;
+    private String username;
+    private String password;
+
+    public db(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+        try {
+            // Initialize the database connection in the constructor
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertUser(String username, String password, String email) {
+        try {
+            // Prepare the insert statement
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO user (username, password, email) VALUES (?, ?, ?)");
+            statement.setString(1, username);
+            statement.setString(2, password);
+            statement.setString(3, email);
+
+            // Execute the insert statement
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getUsers() {
+        try {
+            // Prepare a select statement to get all users
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM user");
+            ResultSet resultSet = statement.executeQuery();
+
+            // Process and print the results
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
+                String created_at = resultSet.getString("created_at");
+
+                System.out.println("User ID: " + id);
+                System.out.println("Username: " + username);
+                System.out.println("Password: " + password);
+                System.out.println("Email: " + email);
+                System.out.println("Created At: " + created_at);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            // Close the database connection
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+  
 };
--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Replace with your actual database URL, username, and password
+        String url = "jdbc:mysql://localhost:3306/DBblog";
+        String username = "root";
+        String password = "morootok";
+
+        // Create a DB object to perform database operations
+        db db = new db(url, username, password);
+
+        // Insert a user
+        db.insertUser("exampleUser", "password123", "user@example.com");
+
+        // Get and print all users
+        db.getUsers();
+
+        // Close the database connection
+        db.closeConnection();
     }
 }
