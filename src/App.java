c7393e7a769b272e602e3af11b10b1a00afcb0df
coder <coder@192.168.1.54>
2023-11-07 23:16:57 +0300

    test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("muntadar, World!");
+        System.out.println("ibrahim, World!");
     }
 }
