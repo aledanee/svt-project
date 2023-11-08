bb77a2b94285c2a7a816c8272fd9c4e3c10199dd
IAMOGTABA <mogtabaoday@gmail.com>
2023-11-08 23:51:29 +0300

    me

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("ibrahim, World!");
  System.out.println("john, World!");
+ System.out.println("df");
     }
 }
